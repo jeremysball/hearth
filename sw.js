f118c80f4b4a53b1626aa63446b80baafaca0c93
jeremysball <jeremystevenball@gmail.com>
2026-07-07 00:15:29 -0400

    chore: bump version

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Hearth PWA service worker
-const VERSION = 'hearth-2026-07-07T03:16Z'; // Must match <meta name="version"> in index.html
+const VERSION = 'hearth-2026-07-07T04:15Z'; // Must match <meta name="version"> in index.html
 const SHELL = [
   './',
   './index.html',
